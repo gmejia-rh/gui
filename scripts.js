b58ec53c49c8b611a77442745859243a1a63c9e9
madgab <angaag@gmail.com>
2014-12-04 19:26:46 +0100

    version updated(from friday 28-11)

--- scripts.js
+++ scripts.js
@@ -3,39 +3,56 @@
 	function pswds() {
 		alert("Admins: ervtod, hirchr, jorass, saskru, svetor Users(examples): aamsta, anddar, ankov");
 	}
-	function loginusr() {
+function loginusr() {
 	var usrname = document.getElementById("login").value;
 	var pswd = document.getElementById("password").value;
 	//alert("usr"+usrname+"pswd"+pswd);
 	var admins = ['ervtod', 'hirchr', 'jorass', 'saskru', 'svetor'];
-	var correct_login  = false;
+	var users = ['aamsta', 'anddar', 'ankov', 'aqulyn'];
+	var correct_login = false;
 	var usr = "";
-	for (var i = 0; i < admins.length; i++){
+	for (var i = 0; i < admins.length; i++) {
 		//alert("testar mot "+admins[i]+" pswd "+pswd);
-		if (pswd === admins[i]){
+		if (pswd === admins[i]) {
 			correct_login = true;
 			usr = admins[i];
 			//alert("login as " + usr);
 		}
 	}
 	if (correct_login == true) {
-		alert("du loggas in som " + usr);
+		//alert("du loggas in som " + usr);
 		sessionStorage.myvar = usr;
 		window.open("Bartender.html");
+	} else {
+		for (var i = 0; i < users.length; i++) {
+			if (pswd === users[i]) {
+				correct_login = true;
+				usr = users[i];
+				//alert("login as " + usr);
+			}
+		}
+		if (correct_login == true) {
+			//alert("du loggas in som " + usr);
+			sessionStorage.myvar = usr;
+			window.open("Customer.html");
+		}
+
 	}
-	//var test = document.getElementByID("fas2");
-	//var test2 = test.value;
-	//alert(" " + test);
-	}
+}
 	
 	
 	
 	//ONLOAD FOR BOTH BARTENDER AND CUSTOMER TO GET THE USERNAME LATER USED 
-	//function onloading(){
-	//	var usr = sessionStorage.myvar;
+	function onloading(){
+		var usr = sessionStorage.myvar;
+		beer_box();
+		var creditslist = JSON.parse(Get("http://pub.jamaica-inn.net/fpdb/api.php?username="+usr+"&password="+usr+"&action=iou_get"));
+//alert ("creditslist<"+creditslist.payload[0].assets+">");
+		var credits_temp = creditslist.payload[0].assets;
+//alert ("credits_temp<"+credits_temp+">");
+		var credits = parseInt(credits_temp, 10);
 
-	//	alert(" hej " + usr);
-	//	}
+		}
 
 //Beer Listbox
 function Get(yourUrl){
@@ -103,10 +120,10 @@ function beer_box(){
 }
 
 // displays the choosen beer with more information
-
-function changeFunc(beer_id){
-	var url='http://pub.jamaica-inn.net/fpdb/api.php?username=ervtod&password=ervtod&action=beer_data_get&beer_id="'+beer_id+'"';
-
+var beer_id;
+function changeFunc(beer_id_temp){
+	var url='http://pub.jamaica-inn.net/fpdb/api.php?username=ervtod&password=ervtod&action=beer_data_get&beer_id="'+beer_id_temp+'"';
+	beer_id = beer_id_temp;
 	var beer_info = JSON.parse(Get(url));
 	document.getElementById("the_selected_beer").innerHTML=beer_info.payload[0].namn;
 	document.getElementById("beer_info").innerHTML=beer_info.payload[0].namn + "-- " + beer_info.payload[0].namn2+"--"+ beer_info.payload[0].leverantor;
@@ -141,4 +158,79 @@ function search_beer(){
 
 	list=[];
 
+}
+
+//////////////////////////////////////////////////////
+// Beginning of the tab management
+/////////////////////////////////////////////////////
+// This function should create the Tab for a costumer
+var ListTab=[];
+var cost = [];
+function createTab(){
+	//ListTab[ListTab.length] = [];
+	cost[cost.length] = 0;
+}
+// Add a beer to the tab
+function addBeerTab(){
+	//alert(beer_id);
+	var id = ListTab.length;
+	//var length=ListTab[id].length;
+	var url='http://pub.jamaica-inn.net/fpdb/api.php?username=ervtod&password=ervtod&action=beer_data_get&beer_id="'+beer_id+'"';
+	var beer_info = JSON.parse(Get(url));
+	var array = [];
+	array[0] = beer_info.payload[0].namn;
+	array[1] = beer_info.payload[0].pub_price;
+
+	ListTab[id][length-1]=array;
+
+	//updateCost();
+	array = []
+}
+function removeBeer(cmd_id){
+	ListTab.remove(cmd_id);
+	updateCost();
+}
+function updateCost(){
+	var sum = 0;
+	for(ii=0;ii<listTab.length-1;ii++){
+		sum += listTab[ii][1];
+	}
+	cost = sum;
+};
+//Creates a listbox with all the beers in the tab.
+function tab_box(){
+	ListTab.sort();
+	var output='<select size="20" name="decision2" style="width: 600px;" onchange="changeFunc(value);">';
+	for(i=0; i<ListTab.length; i++){
+		output += '<option value='+ i +'>';
+
+			output += " " + ListTab[i][0];
+		output += '</option>'+'</br>';
+	}
+//alert("listan" + output)
+	output += '</select>'
+	document.getElementById("tab").innerHTML = output;
+}
+//////////////////////////////////////////////////////
+// End of the tab management
+//////////////////////////////////////////////////////
+
+
+function Get(yourUrl){
+	var Httpreq = new XMLHttpRequest(); // a new request
+	Httpreq.open("GET",yourUrl,false);
+	Httpreq.send(null);
+	return Httpreq.responseText;
+}
+
+function onClick() {
+	var temp = document.getElementById("creditsbox").value;
+	var addvalue = parseInt(temp, 10);
+	credits += addvalue;
+	//alert ("addvalue<"+addvalue+">");
+	document.getElementById("credits").innerHTML = credits;
+};
+
+function logout(){
+	sessionStorage.myvar = "";
 }
