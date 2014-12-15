1025c031a2b9ac2d61c715fc0bdb4c339835a774
madgab <angaag@gmail.com>
2014-12-15 17:52:38 +0100

    Adding some spanish and english words

--- language.js
+++ language.js
@@ -1,7 +1,11 @@
 function changeToSpanish(){
 	document.getElementById('bebidas').innerHTML = "Bebidas";	
+	document.getElementById('elegirBebida').innerHTML = "Por favor, elija una bebida: ";
+	document.getElementById('beer').innerHTML = "Cerveza";
 	}
 	
 function changeToEnglish(){
 	document.getElementById('bebidas').innerHTML = "Beverages";	
+	document.getElementById('elegirBebida').innerHTML = "Please choose a Beverage: ";
+	document.getElementById('buscar').innerHTML = "Search ";
 	}
